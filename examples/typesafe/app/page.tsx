2c454034baf8e84fce3ba5f14cde80d90897f92d
giftlion <netanelac770@gmail.com>
2025-10-15 17:01:41 +0300

    crud in local store

--- examples/typesafe/app/page.tsx
+++ examples/typesafe/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import { DB, where } from "@repo/typesafe";
+import { z } from "zod";
+
+const schema1 = z.object({
+  users: z.object({
+    id: z.number(),
+    name: z.string(),
+    email: z.string(),
+  }),
+  products: z.object({
+    id: z.number(),
+    name: z.string(),
+    price: z.number().min(0),
+  }),
+});
+
+const db = new DB("test1", schema1);
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  db.tables.users.insert({
+    id: 1,
+    name: "A",
+    email: "alice@example.com",
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  db.tables.users.insert({
+    id: 2,
+    name: "B",
+    email: "alice@example.com",
+  });
+
+  const data1 = db.tables.users.query();
+  console.log(data1);
+
+  // db.tables.users.delete((user) => user.id === 1);
+
+  const data2 = db.tables.users.query();
+  console.log(data2);
+
+  db.tables.users.delete(where({ id: 2 }));
+
+  const data3 = db.tables.users.query();
+  console.log(data3);
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Typesafe DB Example</h1>
+      <CompA />
+      <CompB />
     </div>
   );
 }
+
+const CompA = () => {
+  const name = db.tables.users.query()[0]?.name;
+  return <div>{name}</div>;
+};
+
+const CompB = () => {
+  const email = db.tables.users.query()[0]?.email;
+  return <div>{email}</div>;
+};
