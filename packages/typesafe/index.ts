07c5a0e144ea0e5c23a45cfcdb54ee3c5045b234
giftlion <netanelac770@gmail.com>
2025-10-15 17:01:31 +0300

    crud in local store

--- packages/typesafe/index.ts
+++ packages/typesafe/index.ts
@@ -1,13 +1,12 @@
-type BaseDB = {
-  tables: {
-    [key: string]: any;
-  };
-};
+import { z } from "zod";
 
 class Table<Row> {
   private table: Row[] = [];
 
-  public query(): Row[] {
+  public query(predicate?: (item: Row) => boolean): Row[] {
+    if (predicate) {
+      return this.table.filter(predicate);
+    }
     return this.table;
   }
 
@@ -16,25 +15,76 @@ class Table<Row> {
     return item;
   }
 
+  public delete(predicate: (item: Row) => boolean) {
+    const deletedRow = this.table.find(predicate);
+    this.table = this.table.filter((item) => !predicate(item));
+    return deletedRow;
+  }
+
+  public update(predicate: (item: Row) => boolean, newItem: Partial<Row>) {
+    const index = this.table.findIndex(predicate);
+    if (index !== -1) {
+      this.table[index] = { ...this.table[index], ...newItem };
+      return this.table[index];
+    }
+  }
+
   constructor() {
     this.table = [];
   }
 }
-
-export class DB<T extends BaseDB> {
-  //   public data: { [K in keyof T["tables"]]: Array<T["tables"][K]> } = {} as {
-  //     [K in keyof T["tables"]]: Array<T["tables"][K]>;
-  //   };
-
-  public tables: { [K in keyof T["tables"]]: Table<T["tables"][K]> } = {} as {
-    [K in keyof T["tables"]]: Table<T["tables"][K]>;
+export class DB<T extends z.ZodObject> {
+  public tables: { [K in keyof z.infer<T>]: Table<z.infer<T>[K]> } = {} as {
+    [K in keyof z.infer<T>]: Table<z.infer<T>[K]>;
   };
 
-  constructor(public name: string) {
+  constructor(
+    public name: string,
+    private schema: T
+  ) {
     this.name = name;
+    this.schema = schema;
 
-    for (const tableName in null as any as T["tables"]) {
-      this.tables[tableName] = new Table<T["tables"][typeof tableName]>();
+    if (schema) {
+      const shape = schema.shape;
+      for (const tableName of Object.keys(shape) as Array<keyof z.infer<T>>) {
+        this.tables[tableName] = new Table<any>() as Table<
+          z.infer<T>[typeof tableName]
+        >;
+       
+      }
     }
   }
 }
+
+export const where =
+  <A extends Record<string, any>>(a: A) =>
+  (b: A) => {
+    return Object.keys(a).every((key) => a[key] === b[key]);
+  };
+
+const schema = z.object({
+  users: z.object({
+    id: z.number(),
+    name: z.string(),
+    email: z.string(),
+  }),
+  products: z.object({
+    id: z.number(),
+    name: z.string(),
+    price: z.number(),
+  }),
+});
+
+const db1 = new DB("test", schema);
+
+db1.tables.users.query();
+
+db1.tables.users.insert({
+  id: 1,
+  name: "Alice",
+  email: "alice@example.com",
+});
+
+db1.tables.users.delete(where({ id: 1 }));
+db1.tables.users.update(where({ id: 2 }), { name: "Bob" });
---
+++ packages/typesafe/tests.test.ts
@@ -0,0 +1,61 @@
+import { DB, where } from "./index";
+import { z } from "zod";
+import { expect, test } from "vitest";
+
+const schema = z.object({
+  users: z.object({
+    id: z.number(),
+    name: z.string(),
+    email: z.string(),
+  }),
+  products: z.object({
+    id: z.number(),
+    name: z.string(),
+    price: z.number(),
+  }),
+});
+
+test("DB operations", () => {
+  const db = new DB("test", schema);
+
+  const insert1 = db.tables.users.insert({
+    id: 1,
+    name: "Alice",
+    email: "alice@example.com",
+  });
+
+  const insert2 = db.tables.users.insert({
+    id: 2,
+    name: "Bob",
+    email: "bob@example.com",
+  });
+
+  const insert3 = db.tables.users.insert({
+    id: 3,
+    name: "Product A",
+    email: "productA@example.com",
+  });
+
+  expect(db.tables.users.query().length).toBe(3);
+
+  expect(db.tables.users.query(where({ id: 1 })).length).toBe(1);
+  
+  const UpdatedUser = db.tables.users.update(where({ id: 1 }), {
+    name: "Alice Updated",
+  });
+
+  expect(UpdatedUser?.name).toBe("Alice Updated");
+
+  db.tables.users.delete(where({ id: 2 }));
+
+  expect(db.tables.users.query().length).toBe(2);
+
+  db.tables.users.delete(where({ id: 1, name: "Alice Updated" }));
+
+  expect(db.tables.users.query().length).toBe(1);
+
+  //   shouldnt delete because name is different
+  db.tables.users.delete(where({ id: 3, name: "bla bla" }));
+
+  expect(db.tables.users.query().length).toBe(1);
+});
